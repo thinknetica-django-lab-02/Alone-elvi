f514a1f5f7cc5b2edcb7411a2866d743ff39a7c1
Alone-elvi <alone.elvi@gmail.com>
2021-04-03 18:49:54 +0300

    4.1.Фильтрация и пагинация. Начало

--- thinktetika/main/views.py
+++ thinktetika/main/views.py
@@ -4,17 +4,19 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
 
+
 def index(request):
     turn_on_block = True
     data = {'turn_on_block': turn_on_block, 'username': request.user.username}
 
     return render(request, "pages/index.html", data)
 
+
 class GoodsListView(ListView):
     model = Product
     template_name = 'pages/goods.html'
+    paginate_by = 10
 
 class GoodsDetalView(DetailView):
     model = Product
     template_name = 'pages/good-detail.html'
-
