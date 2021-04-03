aae828af32cbbdd222b783f00c63b76a1042534a
Alone-elvi <alone.elvi@gmail.com>
2021-04-03 17:04:22 +0300

    3.4.Настройка представлений просмотра. Добавлены docstrings к классам во view.py. Убрана закомментированная строка в шаблоне goods.html

--- thinktetika/main/views.py
+++ thinktetika/main/views.py
@@ -4,17 +4,21 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
 
+
 def index(request):
     turn_on_block = True
     data = {'turn_on_block': turn_on_block, 'username': request.user.username}
 
     return render(request, "pages/index.html", data)
 
+
 class GoodsListView(ListView):
+    """Класс GoodsListView генерирует список товаров в шаблон pages/goods.html"""
     model = Product
     template_name = 'pages/goods.html'
 
+
 class GoodsDetalView(DetailView):
+    """Класс GoodsListView генерирует описание единицы товара в шаблон pages/good-detail.html"""
     model = Product
     template_name = 'pages/good-detail.html'
-
