cd1e714356435030990c6a186a62e707bc0d0fbe
Alone-elvi <alone.elvi@gmail.com>
2021-04-04 12:18:36 +0300

    4.1.Фильтрация и пагинация

--- thinktetika/main/templatetags/main_tags_filters.py
+++ thinktetika/main/templatetags/main_tags_filters.py
@@ -1,5 +1,8 @@
 from django import template
 from django.utils import timezone
+from django import template
+
+from main.models import Category, Tag
 
 register = template.Library()
 
@@ -15,3 +18,11 @@ def change_first_last_chars_in_string(string):
     """Меняем первый и последний символ строки местами"""
     return string[-1:] + string[1:-1] + string[:1]
 
+
+@register.inclusion_tag('main/snippets/tags_links.html', takes_context=True)
+def goods_tags_links(context) -> dict:
+    """Возвращает список ссылок <a> на тэги товаров"""
+    tags = Tag.objects.all()
+    print(tags)
+    request = context['request']
+    return {'tags': tags, 'request': request}
