015d742c89889d08a0a33d8d14ef1b33f43b0f7c
Alone-elvi <alone.elvi@gmail.com>
2021-05-08 00:05:00 +0300

    13.2.Логгирование

--- thinktetika/thinktetika/settings.py
+++ thinktetika/thinktetika/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from decouple import config
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -238,3 +241,17 @@ TWILIO_AUTH_TOKEN = config('TWILIO_AUTH_TOKEN')
 
 FAKER_LOCALE = None  # settings.LANGUAGE_CODE is loaded
 FAKER_PROVIDERS = None  # faker.DEFAULT_PROVIDERS is loaded (all)
+
+sentry_sdk.init(
+    dsn=config('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
